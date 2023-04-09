6ad8016dbadffac83f2fd7233feb618ee02bb439
Redstonerayy <antonpaulr@protonmail.com>
2023-04-09 23:46:41 +0200

    try catch

--- background.js
+++ background.js
@@ -1,7 +1,10 @@
 // send message to tab if YT url changes
 chrome.webNavigation.onHistoryStateUpdated.addListener((details) => {
     if(details.url.includes("https://www.youtube.com/watch")){
-        chrome.tabs.sendMessage(details.tabId, { navigation: true });
-        console.log("sa;ldfj");
+        try {
+            chrome.tabs.sendMessage(details.tabId, { navigation: true });
+        } catch (e) {
+            // do nothing
+        }
     }
 });
