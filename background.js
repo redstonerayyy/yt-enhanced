db8b6cae7aceb12efa070bbf9ddbfc16ad833d7d
Redstonerayy <antonpaulr@protonmail.com>
2023-04-16 16:30:37 +0200

    working now in firefox

--- background.js
+++ background.js
@@ -1,10 +1,10 @@
 // send message to tab if YT url changes
-chrome.webNavigation.onHistoryStateUpdated.addListener( async (details) => {
-    if(details.url.includes("https://www.youtube.com/watch")){
-        try {
-            await chrome.tabs.sendMessage(details.tabId, { navigation: true });
-        } catch (e) {
-            // do nothing
-        }
-    }
+chrome.webNavigation.onHistoryStateUpdated.addListener(async (details) => {
+	if (details.url.includes("https://www.youtube.com/watch")) {
+		try {
+			await chrome.tabs.sendMessage(details.tabId, { navigation: true });
+		} catch (e) {
+			// do nothing
+		}
+	}
 });
