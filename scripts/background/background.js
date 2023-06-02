11e5b9438c590c1cedb3ccbf794158edde5752bd
Redstonerayy <antonpaulr@protonmail.com>
2023-06-02 19:31:22 +0200

    start restructure

--- scripts/background/background.js
+++ scripts/background/background.js
@@ -1,10 +1,8 @@
-// send message to tab if YT url changes
+/*------------ send message to tab if YT url changes ------------*/
 chrome.webNavigation.onHistoryStateUpdated.addListener(async (details) => {
 	if (details.url.includes("https://www.youtube.com/watch")) {
 		try {
 			await chrome.tabs.sendMessage(details.tabId, { navigation: true });
-		} catch (e) {
-			// do nothing
-		}
+		} catch (e) {}
 	}
 });
