195d95db4ac721053aaf4acc5ffb4b56da10dd8d
Redstonerayy <antonpaulr@protonmail.com>
2023-07-12 10:45:23 +0200

    change structure

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,16 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
-// set browser to brave
-// process.env.BROWSER = "brave";
-
 export default defineConfig({
 	plugins: [svelte({})],
 	// specify other file
 	build: {
-		outDir: "../playlist",
+		outDir: "./playlist",
 	},
 	// alter filepath so it's absolute from extension root
 	base: "/playlist",
-	// open on developement start
-	// server: {
-	// 	open: "playlist.html",
-	// },
 });
+
+// for in browser use
+// process.env.BROWSER = "brave";
+// https://stackoverflow.com/questions/71295772/in-vite-is-there-a-way-to-update-the-root-html-name-from-index-html
