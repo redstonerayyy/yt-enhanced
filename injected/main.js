2c8701f694a3079afe75732a1379a67e63fecf17
Redstonerayy <antonpaulr@protonmail.com>
2023-08-09 15:42:29 +0200

    update ui and settings

--- injected/main.js
+++ injected/main.js
@@ -5,7 +5,34 @@
  -------------------------------------------------*/
 
 (async () => {
-	await preload();
-	await loop();
-	await save();
+	const result = await chrome.storage.local.get(["uioption"]);
+
+	if (result.uioption.option == "full") {
+		await preload();
+		await loop();
+		await save();
+	} else if (result.uioption.option == "save") {
+		await preload();
+
+		/*------------ remove existing ui ------------*/
+		const ui = document.querySelector("#yt-enhanced-ui");
+		ui.remove();
+
+		/*------------ create new save button ------------*/
+		const newsavebutton = document.createElement("img");
+		newsavebutton.src = RES.images["save"];
+		newsavebutton.id = "save-button";
+		newsavebutton.style.height = "26px";
+		newsavebutton.style.width = "28px";
+		newsavebutton.style.margin = "0px 10px";
+		newsavebutton.style.cursor = "pointer";
+
+		const ytmenu = document.querySelector("#actions");
+		ytmenu.append(newsavebutton);
+
+		/*------------ rereference it for the save function ------------*/
+		UI.save.button = document.querySelector("#actions > #save-button");
+
+		await save();
+	}
 })();
--- src/components/Settings.svelte
+++ src/components/Settings.svelte
@@ -20,24 +20,72 @@
 			filename: `yt_enhanced_export_${today}.json`,
 		});
 	}
+
+	/*------------ make user choose ui on youtube.com ------------*/
+	const uioptions = [
+		{ id: "full", text: "Loop and Save" },
+		{ id: "save", text: "Only Save" },
+		{ id: "none", text: "Neither" },
+	];
+
+	let selected = "";
+
+	async function loaduioption() {
+		const result = await chrome.storage.local.get(["uioption"]);
+		selected = result.uioption.option;
+
+		return "";
+	}
+
+	// if (["full", "save", "none"].includes(result.uioption.option)) {
+	// 	selected = result.uioption.option;
+	// }
+
+	async function uichanged() {
+		await chrome.storage.local.set({
+			uioption: { option: selected },
+		});
+	}
 </script>
 
 <div>
-	<button class="export" on:click={exportvideos}>Export</button>
+	<div class="export" on:click={exportvideos}>Export</div>
+	{#await loaduioption() then uiloaded}
+		<select class="ui-options" bind:value={selected} on:change={uichanged}>
+			{#each uioptions as uioption}
+				<option value={uioption.id}>
+					{uioption.text}
+				</option>
+			{/each}
+		</select>
+	{/await}
 </div>
 
 <style>
 	.export {
-		border: 1px solid rgb(63, 63, 63);
+		border: 1px solid rgb(151, 151, 151);
 		border-radius: 5px;
 		font-size: 14px;
 		margin: 20px;
-		padding: 4px;
+		padding: 4px 8px;
 		cursor: pointer;
 		user-select: none;
 	}
 
 	.export:active {
 		background-color: rgb(212, 212, 212);
+	}
+
+	.ui-options {
+		outline: none;
+		border: 1px solid rgb(151, 151, 151);
+		border-radius: 5px;
+		font-size: 14px;
+		margin: 20px;
+		background-color: #181a21;
+		color: #ffffff;
+		padding: 4px 8px;
+		cursor: pointer;
+		user-select: none;
 	}
 </style>
